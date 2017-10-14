c6f427e8ee9f1c26f63847ae2dafd6e1ecd81f22
dreiv <dreivoicu@gmail.com>
2017-10-15 00:03:32 +0300

    thought I commited this :(

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SharedModule } from '@app/shared';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared';
 
 @NgModule({
   declarations: [
