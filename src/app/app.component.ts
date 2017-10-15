e15cf4ca2a1013b245a48dcfc33c883a88324f1f
dreiv <dreivoicu@gmail.com>
2017-10-15 07:28:38 +0300

    cleanup + test CI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>hello world</h1>`,
-  styles: []
+  template: `<h1>hello world</h1>`
 })
 export class AppComponent {
 }
