ae5a56b442938b9b61ea39bcf67da7433cc6db7d
dreiv <dreivoicu@gmail.com>
2017-10-29 20:47:12 +0200

    chart works but can sure use some refining.

--- src/app/features/user-chart/user-chart.component.ts
+++ src/app/features/user-chart/user-chart.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { UsersService } from '../../services/users.service';
+import { UserData } from '../../shared/type/user-data.type';
 import { User } from '../../shared/type/user.type';
 
 @Component({
@@ -10,14 +11,18 @@ import { User } from '../../shared/type/user.type';
 })
 export class UserChartComponent implements OnInit {
   user: User;
+  userData: UserData[];
 
   constructor(private cdr: ChangeDetectorRef,
-              private usersService: UsersService) { }
+              private usersService: UsersService) {
+    this.userData = this.usersService.userData.slice(2);
+  }
 
   ngOnInit() {
     this.usersService.selectedUserChange$
       .subscribe(u => {
         this.user = u;
+
         this.cdr.markForCheck();
       });
   }
