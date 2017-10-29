23431a2a6853f3d7259e2e0c7b329ddb5cd66221
dreiv <dreivoicu@gmail.com>
2017-10-29 21:29:04 +0200

    refined the chart component.

--- src/app/features/user-chart/user-chart.component.ts
+++ src/app/features/user-chart/user-chart.component.ts
@@ -3,6 +3,14 @@ import { UsersService } from '../../services/users.service';
 import { UserData } from '../../shared/type/user-data.type';
 import { User } from '../../shared/type/user.type';
 
+export interface ChartUser extends User {
+  applesOffset: number;
+  bananasOffset: number;
+  kiwisOffset: number;
+  orangesOffset: number;
+  lemonsOffset: number;
+}
+
 @Component({
   selector: 'app-user-chart',
   templateUrl: './user-chart.component.html',
@@ -10,7 +18,7 @@ import { User } from '../../shared/type/user.type';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class UserChartComponent implements OnInit {
-  user: User;
+  user: ChartUser;
   userData: UserData[];
 
   constructor(private cdr: ChangeDetectorRef,
@@ -21,7 +29,14 @@ export class UserChartComponent implements OnInit {
   ngOnInit() {
     this.usersService.selectedUserChange$
       .subscribe(u => {
-        this.user = u;
+        this.user = <ChartUser>u;
+        if (u) {
+          this.user.applesOffset = u.apples * 10;
+          this.user.bananasOffset = u.bananas * 10;
+          this.user.kiwisOffset = u.kiwis * 10;
+          this.user.orangesOffset = u.oranges * 10;
+          this.user.lemonsOffset = u.lemons * 10;
+        }
 
         this.cdr.markForCheck();
       });
