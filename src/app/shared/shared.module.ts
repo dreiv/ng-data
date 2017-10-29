7f7e3291d864ad1596e0cc5167616b730fa25d7b
dreiv <dreivoicu@gmail.com>
2017-10-29 16:36:47 +0200

    done edit flow. Fixed bug.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { CdkTableModule } from '@angular/cdk/table';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import {
   MatButtonModule,
   MatCardModule,
@@ -17,6 +18,7 @@ import { UsersComponent } from '../features/users/users.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
 
     CdkTableModule,
     MatButtonModule,
